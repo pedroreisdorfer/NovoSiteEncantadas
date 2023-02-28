eb871dae1797e804b191c89ef0768e1a5ed3af44
Pedro Reisdorfer <vinireisdorfer@gmail.com>
2023-02-27 22:41:18 -0300

    - novas atualizações para conexão com banco de dados;

---
+++ WebEncantadas/ConnectionManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace WebEncantadas
+{
+    public class ConnectionManager
+    {
+        private static string stringDeConexao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=DB_Encantadas_teste;Data Source=LAPTOP-8RL84DG9\SQLEXPRESS";
+
+
+        public static SqlConnection GetConnection() {
+        
+            var cnnString = new SqlConnection(stringDeConexao);
+            return cnnString;
+        } 
+    }
+}
