95c20cb88a81c6128ee3ff3ba8f73b919f2855db
Pedro Reisdorfer <vinireisdorfer@gmail.com>
2023-04-25 19:52:18 -0300

    - novo contexto

---
+++ WebEncantadas/Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebEncantadas.Models.ViewModels;
+
+namespace WebEncantadas.Data
+{
+    public class Contexto : DbContext
+    {
+        //dbset define os modelos que serão controlados
+        public DbSet<CadastroViewModel> Cadastro { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        // Construtor: My Context recebe DbConetextOptions, que tem como parâmetro o Contexto e devolve um options. Depois do :, ai a classe é passada para a base. Construtor padrão, pois preciso passar Contexto para a Base - injetando options dentro da base.
+
+    }
+}
